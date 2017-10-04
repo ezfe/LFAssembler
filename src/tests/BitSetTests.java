5cd68e68a0664730f40426a5566dcc07e12257ce
Ezekiel Elin <ezekielelin@me.com>
2017-10-04 12:41:08 -0400

    A few more unit tests

--- src/tests/BitSetTests.java
+++ src/tests/BitSetTests.java
@@ -2,6 +2,8 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -23,22 +25,20 @@ public class BitSetTests {
 
 	@Test
 	public void testBitSetByteArray() {
-		byte a = (byte) 0b11001111;
-		byte b = (byte) 0b10111000;
-		byte c = (byte) 0b11011100;
-		byte[] bytes = new byte[]{a, b, c};
-		bits = new BitSet(bytes);
+		byte[] bites = new byte[3];
+		new Random().nextBytes(bites);
+		bits = new BitSet(bites);
 
-		assertEquals("The first bytes should be equal", bits.getByte(0), a);
-		assertEquals("The first bytes should be equal", bits.getByte(1), b);
-		assertEquals("The first bytes should be equal", bits.getByte(2), c);
+		assertEquals("The first bytes should be equal", bits.getByte(0), bites[0]);
+		assertEquals("The first bytes should be equal", bits.getByte(1), bites[1]);
+		assertEquals("The first bytes should be equal", bits.getByte(2), bites[2]);
 	}
 
 	@Test
 	public void testInsertByte() {
-		for(int i = 0; i < 8 * 10; i++) {
-			bits.append(i % 2);
-		}
+		byte[] bites = new byte[10];
+		new Random().nextBytes(bites);
+		bits = new BitSet(bites);
 		
 		byte a = bits.getByte(3);
 		byte a2 = (byte) 0b11110101;
@@ -56,9 +56,9 @@ public class BitSetTests {
 
 	@Test
 	public void testSetByte() {
-		for(int i = 0; i < 8 * 10; i++) {
-			bits.append(i % 2);
-		}
+		byte[] bites = new byte[10];
+		new Random().nextBytes(bites);
+		bits = new BitSet(bites);
 		
 		byte a = bits.getByte(3);
 		byte b = bits.getByte(4);
@@ -113,20 +113,38 @@ public class BitSetTests {
 
 	@Test
 	public void testBytes() {
-		fail("Not yet implemented");
+		byte[] bites = new byte[20];
+		new Random().nextBytes(bites);
+		bits = new BitSet(bites);
+		
+		byte[] found = bits.bytes();
+		
+		for(int i = 0; i < 20; i++) {
+			assertEquals("The " + i + " byte doesn't match", found[i], bites[i]);
+		}
 	}
 
 	@Test
 	public void testGetTrailingLength() {
 		/* This behavior is kinda undefined so not tested */
 		/* assertEquals("The trailing length should start as 8", (Integer)8, bits.getTrailingLength()); */
-		bits.append(0);
-		assertEquals("The trailing length should be 1", (Integer)1, bits.getTrailingLength());
-		bits.append(1);
-		assertEquals("The trailing length should be 2", (Integer)2, bits.getTrailingLength());		
-		for(int i = 0; i < 68; i++) {
-			bits.append(i % 2);
-			assertEquals("The trailing length should be "+i + 3, (Integer)(i + 3), bits.getTrailingLength());
+		for(int i = 0; i < 100; i++) {
+			bits.append(0);
+			assertEquals("The trailing length is wrong", (Integer)1, bits.getTrailingLength());
+			bits.append(1);
+			assertEquals("The trailing length is wrong", (Integer)2, bits.getTrailingLength());		
+			bits.append(0);
+			assertEquals("The trailing length is wrong", (Integer)3, bits.getTrailingLength());		
+			bits.append(1);
+			assertEquals("The trailing length is wrong", (Integer)4, bits.getTrailingLength());		
+			bits.append(1);
+			assertEquals("The trailing length is wrong", (Integer)5, bits.getTrailingLength());		
+			bits.append(0);
+			assertEquals("The trailing length is wrong", (Integer)6, bits.getTrailingLength());		
+			bits.append(0);
+			assertEquals("The trailing length is wrong", (Integer)7, bits.getTrailingLength());		
+			bits.append(0);
+			assertEquals("The trailing length is wrong", (Integer)8, bits.getTrailingLength());
 		}
 	}
 
