8e5f95748984ed20e27ff79d0523a5ea9692cc75
Ezekiel Elin <ezekielelin@me.com>
2017-10-11 20:12:52 -0400

    Cleaned up BitTools and added documentation

--- src/common/BitTools.java
+++ src/common/BitTools.java
@@ -1,26 +1,51 @@
 package common;
 
 public class BitTools {
-	public static Integer bitAt(Integer index, byte data) {
+	/**
+	 * Get the nth bit from the right
+	 * @param index The index (from the right)
+	 * @param data The byte
+	 * @return The bit
+	 */
+	public static int bitAt(int index, byte data) {
 		return (data >> index) & 1;
 	}
 	
-	public static byte setBit(Integer index, byte data, Integer bit) {
+	/**
+	 * Set the nth bit from the right
+	 * @param index The index (from the right)
+	 * @param data The source byte
+	 * @param bit The bit value
+	 * @return The modified byte
+	 * 
+	 */
+	public static byte setBit(int index, byte data, int bit) {
 		if (bit == 0) {
 			return BitTools.setFalse(index, data);
 		} else if (bit == 1) {
 			return BitTools.setTrue(index, data);
 		} else {
-			System.err.println("setBit requires bit to be 1 or 0. Passed " + bit);
-			return data;
+			throw new IllegalArgumentException("Illegal bit value: " + bit);
 		}
 	}
 	
-	public static byte setTrue(Integer index, byte data) {
+	/**
+	 * Set the nth bit from the right to 1
+	 * @param index The index (from the right)
+	 * @param data The source byte
+	 * @return The modified byte
+	 */
+	public static byte setTrue(int index, byte data) {
 		return (byte) (data | (1 << index));
 	}
 	
-	public static byte setFalse(Integer index, byte data) {
+	/**
+	 * Set the nth bit from the right to 0
+	 * @param index The index (from the right)
+	 * @param data The source byte
+	 * @return The modified byte
+	 */
+	public static byte setFalse(int index, byte data) {
 		return (byte) (BitTools.setTrue(index, data) ^ (1 << index));
 	}
 }
